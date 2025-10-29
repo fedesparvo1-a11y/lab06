98f182d7475a049852c37d21a1abbf0fd7ea0b31
Federico Sparvoli <federico.sparvoli@studio.unibo.it>
2025-10-29 20:23:12 +0100

    Completed ex 62

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,9 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.LinkedList;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -21,19 +23,34 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        ArrayList<Integer> list = new ArrayList<Integer>(1000);
+        for (int i = 1000; i < 2000; i++) {
+            list.add(i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        LinkedList<Integer> linkedList = new LinkedList<Integer>(list);
+
+        
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        int first = list.get(0);
+        int last = list.get(list.size() - 1);
+        list.set(0, last);
+        list.set(list.size() - 1, first);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (Integer num : list) {
+            System.out.println(num);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +58,45 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        final int N = 100_000;
+        long time = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            list.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        System.out.println("ArrayList head insert time: " + time + " ns");
+
+        time = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            linkedList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        System.out.println("LinkedList head insert time: " + time + " ns");
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        final int M = 1_000;
+        int middle = list.size() / 2;
+
+        time = System.nanoTime();
+        for (int i = 0; i < M; i++) {
+            list.get(middle);
+        }
+        time = System.nanoTime() - time;
+        System.out.println("ArrayList read time: " + time + " ns");
+
+        middle = linkedList.size() / 2;
+        time = System.nanoTime();
+        for (int i = 0; i < M; i++) {
+            linkedList.get(middle);
+        }
+        time = System.nanoTime() - time;
+        System.out.println("LinkedList read time: " + time + " ns");
+
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -63,8 +113,21 @@ public final class UseListsAndMaps {
          *
          * Oceania -> 38,304,000
          */
+        Map<String, Long> worldPopulation = Map.of(
+            "Africa", 1_110_635_000L,
+            "Americas", 972_005_000L,
+            "Antarctica", 0L,
+            "Asia", 4_298_723_000L,
+            "Europe", 742_452_000L,
+            "Oceania", 38_304_000L
+        );
         /*
          * 8) Compute the population of the world
          */
+        long total = 0;
+        for (long pop : worldPopulation.values()) {
+            total += pop;
+        }
+        System.out.println("World population: " + total);
     }
 }

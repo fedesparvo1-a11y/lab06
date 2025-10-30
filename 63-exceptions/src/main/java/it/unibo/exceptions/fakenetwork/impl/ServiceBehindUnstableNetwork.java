76a4a91586c278c982e82624e070cb61f02d54ba
Federico Sparvoli <federico.sparvoli@studio.unibo.it>
2025-10-30 10:09:30 +0100

    modified the else of the sendData metod

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -55,15 +55,10 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
             commandQueue.add(data);
         } else {
             final var message = data + " is not a valid keyword (allowed: " + KEYWORDS + "), nor is a number";
-            System.out.println(message);
             commandQueue.clear();
-            /*
-             * This method, in this point, should throw an IllegalStateException.
-             * Its cause, however, is the previous NumberFormatException.
-             * Always preserve the original stacktrace!
-             *
-             * The previous exceptions must be set as the cause of the new exception
-             */
+            var ex = new IllegalArgumentException(message);
+            ex.initCause(exceptionWhenParsedAsNumber);  
+            throw ex;
         }
     }
 
